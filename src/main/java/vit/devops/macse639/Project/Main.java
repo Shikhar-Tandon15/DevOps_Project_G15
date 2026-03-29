d01511bbb198312a12a89f6e66d7fed8a2561d8d
Shikhar Tandon <shikhartandon026@gmail.com>
2026-03-29 16:49:05 +0530

    feat: add Main.java integrating all elements

--- src/main/java/vit/devops/macse639/Project/Main.java
+++ src/main/java/vit/devops/macse639/Project/Main.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vit.devops.macse639.Project;
 
-/**
- *
- * @author Shikhar
- */
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Window window = new Window("Paint Canvas");
+        window.open();
+
+        Circle circle = new Circle(5.0f, new Point(100, 100));
+        Rectangle rect = new Rectangle(new Point(10, 10), 80, 60);
+        Polygon poly = new Polygon(new Point[]{
+            new Point(0, 0), new Point(50, 0), new Point(25, 40)
+        });
+
+        window.addShape(circle);
+        window.addShape(rect);
+        window.addShape(poly);
+        window.display();
+
+        window.handleEvent(new Event("CLICK", 150, 200));
+
+        System.out.println("Circle area: " + circle.area(5.0f));
+        System.out.println("Circle circumference: " + circle.circumference());
+
+        ConsoleWindow console = new ConsoleWindow();
+        console.open();
+        console.log("Application started");
+
+        DrawingContext ctx = new DrawingContext(1024, 768);
+        ctx.setPoint(100, 200);
+        ctx.clearScreen();
+
+        DialogBox dialog = new DialogBox("Save", "Save your work?");
+        dialog.showMessage();
+        dialog.submit("my_drawing");
+
+        window.close();
+    }
 }
